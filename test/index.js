5673013e52f2d800471ebd301c1c3ebc5a3f2528
Vadim <narsacgm@gmail.com>
2019-03-22 20:38:05 +0300

    added tests

---
+++ test/index.js
@@ -0,0 +1,7 @@
+// code snippet from https://github.com/30-seconds/30-seconds-of-code#chunk
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size)
+  )
+
+chunk([1, 3, 5, 8], 2)
